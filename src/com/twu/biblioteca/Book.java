b9ef72b36b744cc39fa0ffab3ca54e4d23c42c53
Sasa33 <chengsasa33@gmail.com>
2016-07-24 14:31:18 +0800

    extract Book entity and write test cases

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,41 @@
+package com.twu.biblioteca;
+
+public class Book {
+    private String title;
+    private String author;
+    private String publishYear;
+
+    public Book(String title, String author, String year) {
+        this.title = title;
+        this.author = author;
+        this.publishYear = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(String publishYear) {
+        this.publishYear = publishYear;
+    }
+
+    public String getDetails() {
+        return this.getTitle() + " | " + this.getAuthor() + " | " + this.getPublishYear();
+    }
+}
---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,33 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookTest {
+    private Book book1;
+    private Book book2;
+
+    @Before
+    public void setUp() throws Exception {
+        book1 = new Book("Head First Java", "Kathy Sierra & Bert Bates", "2003");
+        book2 = new Book("Refactoring", "Martin Fowler", "1999");
+    }
+
+    @Test
+    public void should_get_book_each_info_correctly() {
+        assertEquals("Refactoring", book2.getTitle());
+        assertEquals("Martin Fowler", book2.getAuthor());
+        assertEquals("1999", book2.getPublishYear());
+    }
+
+    @Test
+    public void should_get_book_details_correctly() {
+        String details = book1.getDetails();
+
+        String expected = "Head First Java | Kathy Sierra & Bert Bates | 2003";
+
+        assertEquals(expected, details);
+    }
+}
