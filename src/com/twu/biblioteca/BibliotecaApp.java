8762534c95755405ee9e30ed1bfc503e94711333
Sasa33 <chengsasa33@gmail.com>
2016-07-24 12:03:57 +0800

    add test case to show welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,10 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        start();
+    }
+
+    public static void start() {
+        System.out.println("Welcome to the Bangalore Public Library!");
     }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,38 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class BibliotecaAppTest {
+    private BibliotecaApp bibliotecaApp;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    @Test
+    public void should_print_welcome_message_when_the_application_starts() {
+        bibliotecaApp.start();
+
+        String expected = "Welcome to the Bangalore Public Library!\n";
+
+        assertEquals(expected, outContent.toString());
+    }
+}
