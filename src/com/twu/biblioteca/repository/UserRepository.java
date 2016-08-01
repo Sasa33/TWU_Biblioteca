0b14cfa9728809f08124085156fe8f70acd98e2b
Sasa33 <chengsasa33@gmail.com>
2016-08-01 22:42:30 +0800

    add test case to get default users

--- src/com/twu/biblioteca/repository/UserRepository.java
+++ src/com/twu/biblioteca/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.twu.biblioteca.entity.User;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 public class UserRepository {
     private List<User> repository;
 
@@ -11,7 +13,20 @@ public class UserRepository {
         repository = users;
     }
 
+    public UserRepository() {
+
+    }
+
     public List<User> getAllUsers() {
-        return this.repository;
+        if (this.repository != null) {
+            return this.repository;
+        } else {
+            User user1 = new User("user1", "000-0001", "123456", "user1@thoughtworks.com", "13000000000");
+            User user2 = new User("user2", "000-0001", "654321", "user2@thoughtworks.com", "15000000000");
+
+            List<User> users = asList(user1, user2);
+
+            return users;
+        }
     }
 }
--- test/com/twu/biblioteca/repository/UserRepositoryTest.java
+++ test/com/twu/biblioteca/repository/UserRepositoryTest.java
@@ -30,4 +30,16 @@ public class UserRepositoryTest {
         assertEquals(users.get(0).getName(), "user1");
         assertEquals(users.get(1).getName(), "user2");
     }
+
+    @Test
+    public void should_get_default_users_when_no_users_are_set() {
+        repository = new UserRepository();
+
+        List<User> users = repository.getAllUsers();
+
+
+        assertEquals(users.size(), 2);
+        assertEquals(users.get(0).getUserInfo(), "user1 | user1@thoughtworks.com | 13000000000");
+        assertEquals(users.get(1).getUserInfo(), "user2 | user2@thoughtworks.com | 15000000000");
+    }
 }
